9e5f6a7b065529b576fbd7ef8d7f2359d5b42ce3
Riviere Fougy <rivierefougy@gmail.com>
2021-04-23 14:58:10 -0400

    bash.js: created pwd command

---
+++ bash.js
@@ -0,0 +1,12 @@
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  if (cmd === 'pwd') {
+    process.stdout.write(`Test: ${process.cwd()}`);
+  } 
+
+  process.stdout.write('\nprompt > ');
+
+});
